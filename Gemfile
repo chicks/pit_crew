9580448a5346d999cc419764b8aec3a4d7698f96
Carl Hicks <carl.hicks@gmail.com>
2011-01-09 17:54:44 -0800

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+gem "mongoid", "2.0.0.rc.1"
+gem "bson_ext", "1.1.5"
+
+group :test, :development do
+  gem "rspec-rails", "~> 2.4"
+  gem "autotest"
+  gem "capybara"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PitCrew::Application.config.secret_token = '38ed3cadf0025c69a9083d945346b65f096d4f82f6cfc93d66e259416df908f38289c399cce59bc81ecee1cca347a2646f457553da064ca47c1d1c855c4beb03'
